c54c7570f6ddcafe94954f5117d2c6787e07576b
Demieman <137653075+Demieman@users.noreply.github.com>
2024-08-07 01:21:48 +0300

    Add files via upload

---
+++ ClassBased.jsx
@@ -0,0 +1,15 @@
+
+import { Component } from "react";
+class ClassBased extends Component{
+ 
+    render(){
+        return(
+         <div>
+         <h3>hello class based component </h3>
+        </div>
+
+        );
+    }
+}
+
+export default ClassBased;
---
+++ FunctionBased.jsx
@@ -0,0 +1,10 @@
+
+
+function FunctionBased(){
+ return(
+    <div>
+        <h4>functional based componet</h4>
+    </div>
+ );
+}
+export default FunctionBased;
